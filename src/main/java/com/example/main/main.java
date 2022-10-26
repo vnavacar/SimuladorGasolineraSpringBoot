1852f889923967c32bf8e5ba237b9eb770a279ec
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-10-26 09:29:26 +0200

    arreglos

--- src/main/java/com/example/main/main.java
+++ src/main/java/com/example/main/main.java
@@ -3,11 +3,25 @@ package com.example.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.main.Gasolinera.Coche;
+import com.example.main.Gasolinera.Surtidor;
+
+
+
+
 @SpringBootApplication
 public class main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(main.class, args);
+		for(int i = 0; i < 50; i++){
+			Coche coche = new Coche(i);
+			
+		}
+		for(int i= 0; i< 5; i++){
+			Surtidor surtidor = new Surtidor();
+			surtidor.start();
+		}
 	}
 
 }
